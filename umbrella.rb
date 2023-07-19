4b777b6496a4816bbf0f2ff8e9ee3b80a35ec369
mpatel34 <137090752+mpatel34@users.noreply.github.com>
2023-07-19 03:39:45 +0000

    umbrella v1.2

--- umbrella.rb
+++ umbrella.rb
@@ -4,14 +4,14 @@ pp "Howdy"
 
 pp "Where are you?"
 
-#user_location = gets.chomp
+user_location = gets.chomp
 
-user_location = "Chicago"
+# user_location = "Chicago"
 
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("MAPS")
- 
+
 resp = HTTP.get(maps_url)
 
 raw_response = resp.to_s
@@ -31,12 +31,26 @@ location = geo.fetch("location")
 lat = location.fetch("lat")
 long = location.fetch("lng")
 
-pp lat.to_s + long.to_s
-
-
+pp "Your coordinates are " + lat.to_s + long.to_s
 
 weather_key = ENV.fetch("WEATHER") 
 
-private_weather_url = "https://api.pirateweather.net/forecast/" + weather_key + user_location
+private_weather_url = "https://api.pirateweather.net/forecast/" + weather_key + "/" + lat.to_s + "," + long.to_s
 
-pp raw_response = HTTP.get(private_weather_url)
+resp_weather = HTTP.get(private_weather_url)
+
+raw_response_weather = resp_weather.to_s
+
+parsed_response_weather = JSON.parse(raw_response_weather)
+
+currently = parsed_response_weather.fetch("currently")
+
+temperature = currently.fetch("temperature")
+
+pp "It is currently " + temperature.to_s
+
+if currently.fetch("precipProbability") <= 10
+  pp "Suck it up. You will be fine. Even if it did rain, it won't hurt... chill."
+else
+  pp "Bring an umbrella. Seriously. Perhaps even a poncho."
+end
